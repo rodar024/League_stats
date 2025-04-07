4469e9925966cc5749a0ac35faea266abc541fa0
rodar024 <rodartee.m@gmail.com>
2025-04-07 16:19:31 -0700

    progress

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QAction, QIcon, QKeySequence
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QCheckBox,
+    QStatusBar,
+    QToolBar,
+    
+)
+
+from layout_colorwidget import Color
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+
+        label = QLabel("Hello!")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(label)
+
+        toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16, 16))
+        self.addToolBar(toolbar)
+        
+        button_action = QAction(QIcon("bug.png"), "Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.toolbar_button_clicked)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+
+        self.setStatusBar(QStatusBar(self))
+
+    def toolbar_button_clicked(self, s):
+        print("click", s)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
